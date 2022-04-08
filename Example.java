cdc4c71ce356919dd08169cdc54be1e59ef22080
pavanabindu <94432228+pavanabindu@users.noreply.github.com>
2022-04-08 15:48:26 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,41 @@
+import java.sql.*;
+import java.util.*;
+class Example
+{
+ public static void main(String args[])
+ {
+  try
+  {
+   Class.forName("oracle.jdbc.driver.OracleDriver");
+   System.out.println("Loaded");
+   Connection con=DriverManager.getConnection("jdbc:oracle:thin:@ntsocietyserver:1521:oralbrce","IPC96","IPC96");
+     System.out.println("Connection Success");
+     Scanner sc=new Scanner(System.in);
+      System.out.println("Enter your employee id");
+      int empno=sc.nextInt();
+      Statement sct=con.createStatement();
+      String sql="select*from employee";
+   ResultSet rs=sct.executeQuery(sql);
+    boolean result=false;
+     while(rs.next())
+     {
+     //check the rows line by line until all the records are completed
+     //verify input empno with first colun of your table
+ 
+     if(empno==rs.getInt(1))
+     {
+       System.out.println("You are a valid user");
+       System.out.println("Hello"+rs.getString(2));
+       result=true;
+       break;
+  }
+  }//while-closing
+  else
+  System.out.println("invalid user");
+ }//try
+  catch(Exception e)
+  {
+   System.out.println("e");
+  }
+ }
+}
